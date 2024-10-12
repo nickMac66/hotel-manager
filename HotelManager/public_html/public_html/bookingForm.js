48dd887400641bc1f6d1f36e8727cd84bfa062a3
nicmac <nick.mac.dev@outlook.com>
2024-10-12 01:18:33 -0300

    Set index.html as base template; implement dynamic booking form content in bookingForm.js

---
+++ HotelManager/public_html/public_html/bookingForm.js
@@ -0,0 +1,15 @@
+/*
+ * Name: bookingForm.js
+ * Description: JavaScript file to generate a dynamic booking form
+ * Author: NicMac
+ * Created On: October 12, 2024
+ * Last Modified: October 12, 2024
+ */
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("The DOM is fully loaded and parsed!");
+
+    let bookingFormHtml = document.querySelector(".container");
+    bookingFormHtml.innerHTML = 'SAMPLE TEXT';
+
+});
