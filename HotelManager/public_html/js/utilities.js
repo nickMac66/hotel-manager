cc1f4954b0d2ffaa867563bb3653ca1f056bb13d
nicmac <nick.mac.dev@outlook.com>
2024-10-15 23:19:15 -0300

    Added logic to the formGenerator function to create radio buttons

--- HotelManager/public_html/js/utilities.js
+++ HotelManager/public_html/js/utilities.js
@@ -7,6 +7,9 @@
  */
 
 function formGenerator(formData) {
+
+
+
     let tableHeader = " "; // Initialize an empty table header string
 
     // Start building the form
@@ -16,49 +19,74 @@ function formGenerator(formData) {
 
     // Loop through each key in the formData object
     for (let obj in formData) {
-        
+
         let formField = obj; // Get the current form field        
         let nestedObject = formData[obj]; // Get the nested object for the current form field
         let keys = Object.keys(nestedObject);
-        let inputId = keys[0];
+        let firstKey = keys[0];
 
         // Loop through each key in the nested object
         for (let key in nestedObject) {
             let value = formData[formField][key]; // Get the value for the current key in the nested object
 //            console.log(key);
-//            console.log(value);                         
-            
+//            console.log(value); 
+
             if (key == "nextRow") {
                 // If the key is "nextRow", close the current table row
                 form += "</tr>";
-            } else if (key == "inputType") {
+            }
+
+            if (key == firstKey) {
+                // Create a table header with the value
+                form += "<th>" + value + "</th>";
+            }
+
+            if (key == "inputType") {
                 // If the key is "inputType", start a new table cell
                 form += "<td>";
-                
+
                 // Use a switch statement to handle different input types
                 switch (value) {
-                    case "text":                        
-                        form += `<input type="text" id="${inputId}" name="${inputId}">`
-                        console.log(inputId);
+
+                    case "text":
+                        form += `<input type="text" id="${firstKey}" name="${firstKey}">`
+//                        console.log(firstKey);
                         break;
-                    case "date":                        
-                        form += `<input type="date" id="${inputId}" name="${inputId}">`
+
+                    case "date":
+                        form += `<input type="date" id="${firstKey}" name="${firstKey}">`
                         break;
-                    case "radio":                        
-                        form += `<input type="radio" id="${inputId}" name="${inputId}">`
+
+                    case radio:
+                        let name = "";
+                        let value = "";
+
+                        for (let obj in radio) {
+                            let id = obj;
+
+                            for (let key in radio[id]) {
+                                if (key == "name") {
+                                    let name = radio[id][key];
+                                } else {
+                                    let value = radio[id][key];
+                                    form += `<input type="radio" id="${id}" name="${name} value="${value}"`;
+                                    form += `<label for="${id}">${value}</label><br>`;
+                                }
+                            }
+                        }
                         break;
-                    case "submit":                       
+
+                    case "submit":
                         form += "<input type=submit>";
                         break;
+
                     default:
                         // Default case to handle unknown input types, create a text input field
-                        form += `<input type="text" id="${inputId}" name="${inputId}">`
+                        form += `<input type="text" id="${firstKey}" name="${firstKey}">`
                         break;
                 }
+
                 form += "</td>"; // Close the table cell
-            } else {
-                // For other keys, create a table header with the value
-                form += "<th>" + value + "</th>";
             }
         }
     }
