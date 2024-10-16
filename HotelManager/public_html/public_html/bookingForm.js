97ed0ff73d5b7c223a94af081c10e45e3a23aaed
nicmac <nick.mac.dev@outlook.com>
2024-10-15 23:18:20 -0300

    Updated form structure with radio input options

---
+++ HotelManager/public_html/public_html/bookingForm.js
@@ -0,0 +1,34 @@
+/*
+ * Name: bookingForm.js
+ * Description: JavaScript file to generate a dynamic booking form
+ * Author: NicMac
+ * Created On: October 12, 2024
+ * Last Modified: October 12, 2024
+ */
+
+// input id is the object
+const radio = {
+    basic: {name: "roomType", value: "Basic"},
+    deluxe: {name: "roomType", value: "Deluxe"},
+    luxury: {name: "roomType", value: "Luxury"},
+};
+
+const form = {
+    cell1: {fname: "First name ===>", inputType: "text"},
+    cell2: {lname: "Last name ===>", inputType: "text", nextRow: true},
+    cell3: {phone: "Phone ===>", inputType: "text"},
+    cell4: {email: "Email ===>", inputType: "text", nextRow: true},
+    cell5: {checkin: "Check in", inputType: "date"},
+    cell6: {checkout: "Check out", inputType: "date", nextRow: true},
+    cell7: {roomType: "Room type", inputType: radio},
+    cell8: {inputType: "submit"}
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    let bookingFormHtml = document.querySelector(".container");
+    if (bookingFormHtml) {
+        bookingFormHtml.innerHTML = formGenerator(form);
+    } else {
+        console.error("No container found.");
+    }
+});
