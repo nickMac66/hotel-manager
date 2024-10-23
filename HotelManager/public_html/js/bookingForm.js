d83e61822fd8b79c57a6f5888d7119eb6acbc885
nicmac <nick.mac.dev@outlook.com>
2024-10-23 15:52:02 -0300

    Capturing user input on form submission and dynamically displaying it on the index page. Room type input is still pending

--- HotelManager/public_html/js/bookingForm.js
+++ HotelManager/public_html/js/bookingForm.js
@@ -10,10 +10,10 @@
 const radio = {
     basic: {name: "roomType", value: "Basic"},
     deluxe: {name: "roomType", value: "Deluxe"},
-    luxury: {name: "roomType", value: "Luxury"},
+    luxury: {name: "roomType", value: "Luxury"}
 };
 
-const form = {
+const formData = {
     cell1: {fname: "First name ===>", inputType: "text"},
     cell2: {lname: "Last name ===>", inputType: "text", nextRow: true},
     cell3: {phone: "Phone ===>", inputType: "text"},
@@ -25,10 +25,59 @@ const form = {
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-    let bookingFormHtml = document.querySelector(".container");
-    if (bookingFormHtml) {
-        bookingFormHtml.innerHTML = formGenerator(form);
-    } else {
-        console.error("No container found.");
-    }
+
+    let pageContent = document.querySelector(".container");
+
+    pageContent.innerHTML = formGenerator(formData);
+
+    document.getElementById("form").addEventListener("submit", function (event) {
+
+        // Get the booking form
+        let bookingForm = document.getElementById("form");
+
+        // Get values from user input
+        let fname = bookingForm.elements['fname'];
+        fname = fname.value;
+        // Log the first name
+        console.log(`first name: ${fname}`);
+
+        let lname = bookingForm.elements['lname'];
+        lname = lname.value;
+        // Log the last name
+        console.log(`last name: ${lname}`)
+
+        let phone = bookingForm.elements['phone'];
+        phone = phone.value;
+        // Log the phone number
+        console.log(`Phone: ${phone}`);
+
+        let email = bookingForm.elements['email'];
+        email = email.value;
+        // Log the email address
+        console.log(`Email: ${email}`);
+
+        let checkin = bookingForm.elements['checkin'];
+        checkin = checkin.value;
+        // Log the check in date
+        console.log(`Check in: ${checkin}`);
+
+        let checkout = bookingForm.elements['checkout'];
+        checkout = checkout.value;
+        // Log the check out date
+        console.log(`Check out: ${checkout}`);
+
+//        let roomType = bookingForm.elements['roomType'];
+//        roomType = roomType.value;
+//        // Log the check out date
+//        console.log(`Room type: ${roomType}`);
+
+        pageContent.innerHTML = `First name: ${fname}<br>`;
+        pageContent.innerHTML += `Last name: ${lname}<br>`;
+        pageContent.innerHTML += `Phone: ${phone}<br>`;
+        pageContent.innerHTML += `Email: ${email}<br>`;
+        pageContent.innerHTML += `Check in: ${checkin}<br>`;
+        pageContent.innerHTML += `Check out: ${checkout}<br>`;
+        pageContent.innerHTML += `Room type: ${roomType}<br>`;
+        
+    });
 });
