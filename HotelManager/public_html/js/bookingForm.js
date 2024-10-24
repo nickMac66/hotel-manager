e5a5ffcbf07cb577257c4ec17b920862a3a74b6a
nicmac <nick.mac.dev@outlook.com>
2024-10-24 14:55:56 -0300

    Cleaned up code and updated booking form structure

--- HotelManager/public_html/js/bookingForm.js
+++ HotelManager/public_html/js/bookingForm.js
@@ -14,55 +14,20 @@ const radio = {
 };
 
 const formData = {
-    cell1: {fname: "First name ===>", inputType: "text"},
-    cell2: {lname: "Last name ===>", inputType: "text", nextRow: true},
-    cell3: {phone: "Phone ===>", inputType: "text"},
-    cell4: {email: "Email ===>", inputType: "text", nextRow: true},
-    cell5: {checkin: "Check in", inputType: "date"},
-    cell6: {checkout: "Check out", inputType: "date", nextRow: true},
-    cell7: {roomType: "Room type", inputType: radio},
-    cell8: {inputType: "submit"}
+    fname: {value: "First name ===>", inputType: "text", name: "fname"},
+    lname: {value: "Last name ===>", inputType: "text", name: "lname", nextRow: true},
+    phone: {value: "Phone ===>", inputType: "text", name: "phone"},
+    email: {value: "Email ===>", inputType: "text", name: "email", nextRow: true},
+    checkin: {value: "Check in", inputType: "date", name: "checkin"},
+    checkout: {value: "Check out", inputType: "date", name: "checkout", nextRow: true},
+    roomType: {value: "Room type", inputType: radio, name: "roomType"},
+    submit: {inputType: "submit", name: "submit", action: "bookingDetails.html"}
 };
 
 document.addEventListener("DOMContentLoaded", function () {
 
     // Add dynamic page content
-    let pageContent = document.querySelector(".container");
+    let pageContent = document.getElementById("home");
     pageContent.innerHTML = formGenerator(formData);
 
-    document.getElementById("form").addEventListener("submit", function (event) {
-        
-        // Prevent default form action
-        event.preventDefault();
-                
-        // Get references to the booking form
-        let bookingForm = document.getElementById("form");
-        
-        // Get references to the booking form input fields
-        let fname = bookingForm.elements['fname'];
-        let lname = bookingForm.elements['lname'];
-        let phone = bookingForm.elements['phone'];
-        let email = bookingForm.elements['email'];
-        let checkin = bookingForm.elements['checkin'];
-        let checkout = bookingForm.elements['checkout'];
-        let roomType = bookingForm.elements['roomType'];
-
-        // Assign the input field values to variables
-        fname = fname.value;        
-        lname = lname.value;       
-        phone = phone.value;
-        email = email.value;
-        checkin = checkin.value;
-        checkout = checkout.value;
-        roomType = roomType.value;
-
-        // Display user input data
-        pageContent.innerHTML = `First name: ${fname}<br>`;
-        pageContent.innerHTML += `Last name: ${lname}<br>`;
-        pageContent.innerHTML += `Phone: ${phone}<br>`;
-        pageContent.innerHTML += `Email: ${email}<br>`;
-        pageContent.innerHTML += `Check in: ${checkin}<br>`;
-        pageContent.innerHTML += `Check out: ${checkout}<br>`;
-        pageContent.innerHTML += `Room type: ${roomType}<br>`;        
-    });
 });
