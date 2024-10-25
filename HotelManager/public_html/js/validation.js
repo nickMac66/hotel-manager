76191bee39258d85c0abeb0e7f87604cd8a18b3a
nicmac <nick.mac.dev@outlook.com>
2024-10-25 16:16:51 -0300

    Capturing user input data. Next steps will be to validate the data

--- HotelManager/public_html/js/validation.js
+++ HotelManager/public_html/js/validation.js
@@ -1,6 +1,48 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+/**
+ * Name: bookingForm.js
+ * Description: JavaScript file to validate user input 
+ * Author: NicMac
+ * Created On: October 24, 2024
  */
+
+function validateForm() {
+
+    console.log("validating...");
+    
+    let isValid = true;
+
+    // Create a reference to the booking form
+    let bookingForm = document.getElementById("form");
+
+    // Create references to the booking form input fields
+    let fname = bookingForm.elements['fname'];
+    let lname = bookingForm.elements['lname'];
+    let phone = bookingForm.elements['phone'];
+    let email = bookingForm.elements['email'];
+    let checkin = bookingForm.elements['checkin'];
+    let checkout = bookingForm.elements['checkout'];
+    let roomType = bookingForm.elements['roomType'];
+
+    // Assign the user input data to variables
+    fname = fname.value;    
+    lname = lname.value;
+    phone = phone.value;
+    email = email.value;
+    checkin = checkin.value;
+    checkout = checkout.value;
+    roomType = roomType.value;
+
+    return false;
+}
+
+
+
+
+
+
+
+
+
+
 
 
