b4e763560a537b12f01dbe5083360cc683960230
nicmac <nick.mac.dev@outlook.com>
2024-10-31 13:48:58 -0300

    Adding logic to check at least one radio button was selected and displaying an error message if no seletion

--- HotelManager/public_html/js/validation.js
+++ HotelManager/public_html/js/validation.js
@@ -7,29 +7,51 @@
 
 function validateForm() {
 
-    let isValid = true;
+    // Variables to hold input field attributes and values
     let inputLabel = "";
-    let userInput = "";
-    let isEmpty = "";
+    let inputType = "";
+    let inputId = "";
+    let inputName = "";
+    let userInput = "";    
+    let isValid = true;
 
-    const formEntries = [];
+    // Get the user input & associated labels from the form
+    $("#myForm th, #myForm input").each(function () {
 
-    $("#myForm th, #myForm input").each(function (index) {
+        
+        // Boolean variables for validation checks
+        const isRadioInput = (inputType === "radio"); // Checks is the input type is radio
+        const radioIsUnchecked = ($(`input[name='${inputName}']:checked`).length === 0); // Checks if no radio buttons are checked
+        const emptyFields = (`${userInput}` === ""); // Checks if the user input is empty
+        const isNotSubmitButton = (`${inputType}` !== "submit"); // Checks if the input type is not submit
 
+        // Input labels
         if ($(this).is("th")) {
 
             inputLabel = $(this).text();
 
-        }
+        // User input
+        } else {
 
-        if ($(this).is("input")) {
-
-            userInput = $(this).val();
+            // Current input field attributes
+            inputType = $(this).attr('type');
             inputId = $(this).attr('id');
+            inputName = $(this).attr('name');
+            userInput = $(this).val();
 
-            if (userInput === isEmpty) {
+            // Check for empty
+            if (emptyFields && isNotSubmitButton) {
 
-                $(`h5#${inputId}`).html(`${inputLabel} is required`).css("color", "red");
+                // Empty field error message
+                $(`h5#${inputId}`).html(`**${inputLabel} is required`).css("color", "red");
+                isValid = false;
+
+            }
+
+            if (isRadioInput && radioIsUnchecked) {
+
+                // No radio checked error message
+                $("h5#radioError").html(`**${inputLabel} is required`).css("color", "red");
                 isValid = false;
 
             }
@@ -39,3 +61,49 @@ function validateForm() {
     return isValid;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            console.log("field: " + inputName + "is valid?: " + isValid);
+//
+//            // Check for empty fields
+//            if (userInput === isEmpty && inputType !== "radio") {
+//
+//                // Empty field error message
+//                $(`h5#${inputId}`).html(`**${inputLabel} is required`).css("color", "red");
+//
+//                // The form is not valid
+//                isValid = false;
+//
+//            } else if (inputType === "radio") {
+//
+//                inputName = $(this).attr('name');
+////                console.log("radio input name: " + inputName);
+//
+//                if ($(inputName).prop("checked", true)) {
+//
+//                    console.log("a selection has been made");
+//
+//                } else {
+//
+//                    console.log("no selection has been made");
+//                    
+//                    isValid = false;
+//
+//                }
+//
+//            }
