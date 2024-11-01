1d9e490343d2e4b12c764b69b224f7cbd81caf70
nicmac <nick.mac.dev@outlook.com>
2024-11-01 14:06:01 -0300

    Refactoring code for simplicity and readability

--- HotelManager/public_html/js/formHandler.js
+++ HotelManager/public_html/js/formHandler.js
@@ -6,39 +6,24 @@
  * Created On: October 16, 2024 
  */
 
-document.addEventListener("DOMContentLoaded", function () {
+$( document ).ready(function() {
 
-    document.getElementById("myForm").addEventListener("submit", function (event) {
+    $("#myForm").submit(function (event) {
 
-        formIsValid = validateForm();                        
+        formIsValid = validateForm();
+        console.log(formIsValid);
 
         if (formIsValid) {
+
             // Submit the form
             alert("the form is valid");
 
         } else {
+
             // Prevent the default form action
             event.preventDefault();
             alert("the form is not valid");
+
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
-//    let bookingDetails = document.getElementById("bookingDetails");
-//    // Display user input data
-//    bookingDetails.innerHTML = `First name: ${fname}<br>`;
-//    bookingDetails.innerHTML += `Last name: ${lname}<br>`;
-//    bookingDetails.innerHTML += `Phone: ${phone}<br>`;
-//    bookingDetails.innerHTML += `Email: ${email}<br>`;
-//    bookingDetails.innerHTML += `Check in: ${checkin}<br>`;
-//    bookingDetails.innerHTML += `Check out: ${checkout}<br>`;
-//    bookingDetails.innerHTML += `Room type: ${roomType}<br>`;
