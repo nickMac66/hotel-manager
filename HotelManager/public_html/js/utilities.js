9a84cdaa2be632a5a17b440db17e735283520d9f
nicmac <nick.mac.dev@outlook.com>
2024-11-02 01:36:58 -0300

    Simplifying code in the generateForm function

--- HotelManager/public_html/js/utilities.js
+++ HotelManager/public_html/js/utilities.js
@@ -6,113 +6,259 @@
  * Last Modified: October 13, 2024
  */
 
-function formGenerator(formData, formAction) {
+function generateForm(formFields, formFieldsPerRow, formAction) {
 
     // Start building the form
     let form = `<form id="myForm" action="${formAction}">`;
     form += '<table>';
     form += '<tr>';
 
-    // Loop through each form field
-    for (let formField in formData) {
+    // Access the current form field
+    for (let formField in formFields) {
 
-        // Get the form field object containing the input field attributes
-        let formFieldObject = formData[formField];
+        // Current form field attributes
+        let label = "";
+        let inputType = "";
+        let id = "";
+        let name = "";
 
-        // Loop through the keys in the current form field object
-        for (let key in formFieldObject) {
+        // Get the current form field as an object
+        let formFieldObject = formFields[formField];
 
-            // Get the value for the current key in the form field object
-            let value = formData[formField][key];
+        // Access the attributes of the current form field
+        for (let fieldAttribute in formFieldObject) {
 
-            if (key == "nextRow") {
+            // Get the current attribute value
+            let fieldAttributeValue = formFields[formField][fieldAttribute];
 
-                // Close the current table row
-                form += "</tr>";
-            }
+            switch (fieldAttribute) {
 
-            if (key == "value") {
+                case "label":
 
-                // Create a table header with the value
-                form += "<th>" + value + "</th>";
-            }
+                    label = fieldAttributeValue;
+                    break;
 
-            if (key == "inputType") {
+                case "inputType":
 
-                // Start a new table cell
-                form += "<td>";
+                    inputType = fieldAttributeValue;
+                    break;
 
-                // Handle the different input types
-                switch (value) {
+                case "id":
 
-                    case "text":
+                    id = fieldAttributeValue;
+                    break
 
-                        form += `<input type="text" id="${formField}" name="${formField}">`;
-                        // Error message for invalid user input
-                        form += `<h5 id="${formField}"> </h5>`;
-                        break;
+                case "name":
 
-                    case "date":
+                    name = fieldAttributeValue;
+                    break;
 
-                        form += `<input type="date" id="${formField}" name="${formField}">`;
-                        // Error message for invalid user input
-                        form += `<h5 id="${formField}"> </h5>`;
-                        break;
+                default:
 
-                    case radio:
+                    alert("invalid field attribute");
+                    break;
 
-                        // Get the radio fields
-                        for (let radioField in radio) {
-
-                            // Create a reference to the current radio field object
-                            let radioFieldObject = radio[radioField];
-                            let nameAttribute = "";
-                            let valueAttribute = "";
-
-                            // Get the keys from the current radio field object
-                            for (let key in radioFieldObject) {
-
-                                if (key == "name") {
-
-                                    // Set the name attribute for the current radio field
-                                    nameAttribute = radioFieldObject[key];
-
-                                } else {
-
-                                    // Set the value attribute for the current radio field
-                                    valueAttribute = radioFieldObject[key];
-                                    form += `<input type="radio" id="${radioField}" name="${nameAttribute}" value="${valueAttribute}"`;
-
-                                    // Error message for no radio option selected
-
-                                    form += `<label for="${radioField}">${valueAttribute}</label><br>`;
-                                }                              
-                            }                                                                                    
-                        }
-                        
-                        form += `<h5 id="${formField}"></h5>`;
-                        
-                        break;
-
-                    case "submit":
-                        formAction =
-                                form += '<input type="submit" name="submit">';
-                        break;
-
-                    default:
-                        // Default case to handle unknown input types, create a text input field
-                        form += '<input type="text" id="default" name="default">';
-                        break;
-                }
-
-                form += "</td>"; // Close the table cell           
             }
         }
+
+        form += `<th>${label}</th>`;
+        form += '<td>';
+        form += `<input type="${inputType}" id="${id}" name="${name}">`;
+        form += `<h5 id="${id}"></h5>`;
+        form += '</td>';
+        form += '</tr>';
+
     }
 
-    form += "</table>";
-    form += "</form>";
+    // Add the submit button
+    form += '<tr>';
+    form += '<td>';
+    form += '<input type="submit" name="submit">';
+    form += '</td>';
+    form += '</tr>';
+
+    form += '</table>';
+    form += '</form>';
 //    console.log(form);
-    return form; // Return the completed form
+    return form;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///*
+// * Name: utilities.js
+// * Description: JavaScript file containing utility functions for various purposes
+// * Author: NicMac
+// * Created On: October 13, 2024
+// * Last Modified: October 13, 2024
+// */
+//
+//function generateForm(formFields, formAction) {
+//
+//    // Start building the form
+//    let form = `<form id="myForm" action="${formAction}">`;
+//    form += '<table>';
+//    form += '<tr>';
+//
+//    // Access the current form field
+//    for (let formField in formFields) {
+//
+//        // Define the current form field as an object
+//        let formFieldObject = formFields[formField];
+//
+//        // Get the attributes from the current form field
+//        for (let fieldAttribute in formFieldObject) {
+//
+//            // Define the attribute values
+//            let fieldAttributeValue = formFields[formField][fieldAttribute];
+//
+//            if (fieldAttribute == "nextRow") {
+//
+//                // Close the current table row
+//                form += "</tr>";
+//            }
+//
+//            if (fieldAttribute == "value") {
+//
+//                // Create a table header with the value
+//                form += "<th>" + fieldAttributeValue + "</th>";
+//            }
+//
+//            if (fieldAttribute == "inputType") {
+//
+//                // Start a new table cell
+//                form += "<td>";
+//
+//                // Handle the different input types
+//                switch (fieldAttributeValue) {
+//
+//                    case "text":
+//
+//                        form += `<input type="text" id="${formField}" name="${formField}">`;
+//                        // Error message for invalid user input
+//                        form += `<h5 id="${formField}"> </h5>`;
+//                        break;
+//
+//                    case "date":
+//
+//                        form += `<input type="date" id="${formField}" name="${formField}">`;
+//                        // Error message for invalid user input
+//                        form += `<h5 id="${formField}"> </h5>`;
+//                        break;
+//
+//                    case radio:
+//
+//                        // Get the radio fields
+//                        for (let radioField in radio) {
+//
+//                            // Create a reference to the current radio field object
+//                            let radioFieldObject = radio[radioField];
+//                            let nameAttribute = "";
+//                            let valueAttribute = "";
+//
+//                            // Get the keys from the current radio field object
+//                            for (let fieldAttribute in radioFieldObject) {
+//
+//                                if (fieldAttribute == "name") {
+//
+//                                    // Set the name attribute for the current radio field
+//                                    nameAttribute = radioFieldObject[fieldAttribute];
+//
+//                                } else {
+//
+//                                    // Set the value attribute for the current radio field
+//                                    valueAttribute = radioFieldObject[fieldAttribute];
+//                                    form += `<input type="radio" id="${radioField}" name="${nameAttribute}" value="${valueAttribute}"`;
+//
+//                                    // Error message for no radio option selected
+//
+//                                    form += `<label for="${radioField}">${valueAttribute}</label><br>`;
+//                                }                              
+//                            }                                                                                    
+//                        }
+//                        
+//                        form += `<h5 id="${formField}"></h5>`;
+//                        
+//                        break;
+//
+//                    case "submit":
+//                        formAction =
+//                                form += '<input type="submit" name="submit">';
+//                        break;
+//
+//                    default:
+//                        // Default case to handle unknown input types, create a text input field
+//                        form += '<input type="text" id="default" name="default">';
+//                        break;
+//                }
+//
+//                form += "</td>"; // Close the table cell           
+//            }
+//        }
+//    }
+//
+//    form += "</table>";
+//    form += "</form>";
+////    console.log(form);
+//    return form; // Return the completed form
+//
+//}
+//
+//function generateLabels () {
+//    
+//    
+//    
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
