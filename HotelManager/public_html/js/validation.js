cfcfbecbaac1bc9989d0d083e599c9bb79c90c6f
nicmac <nick.mac.dev@outlook.com>
2024-11-05 23:30:37 -0400

    Adding phone number validation in the validateForm function

--- HotelManager/public_html/js/validation.js
+++ HotelManager/public_html/js/validation.js
@@ -5,50 +5,77 @@
  * Created On: October 24, 2024
  */
 
+let isValid = true;
+
 function validateForm() {
 
-    const noRadioSelected = $(`input[name='${name}']:checked`).length === 0;
+    // Constants to validate empty fields
+    const noRadioSelected = $(`input[name='${name}']:checked`).length === 0; // Check for no radio selection     
 
-    let isValid = true;
-    let inputLabel = "";
-//    let radioSelection = $(`input[type='radio'][name='roomType']:checked`).val();   
-
+    // Field label
+    let label = "";
 
     // Get user input & associated labels from the form
     $("#myForm th, #myForm input").each(function () {
 
-        // Constants to check various conditions        
-        const emptyFields = ""; // Check for empty input fields
-        const isLabel = ($(this).is("th")); // Check if input label
+        // Checks if the current attribute is 'label'
+        const isLabel = ($(this).is("th")); // Check if input labe
 
         // Variables to hold input field attributes and values        
-        let type = $(this).attr('type');
         let id = $(this).attr('id');
         let name = $(this).attr('name');
         let group = $(this).attr('group');
-        let userResponse = $(this).val();
+        let userInput = $(this).val();
+
+        // Checks if the current input field is for a phone number
+        let isPhone = ((id) === "phone");
+        let isEmail = ((id) === "email");
+
+        // Checks for empty fields
+        let emptyFields = (userInput === "");
 
         // Label the input fields
         if (isLabel) {
 
-            inputLabel = $(this).text();
+            label = $(this).text();
 
-        } else {
+        }
 
-            // Handle the different input element errors
-            switch (userResponse) {
+        if (emptyFields) {
 
-                case emptyFields:
+            $(`h5#${id}`).html(`**${label} is required`).css("color", "red");
+            isValid = false;
 
-                    $(`h5#${id}`).html(`**${inputLabel} is required`).css("color", "red");
-                    isValid = false;
-                    break;
+        }
+
+        if (isPhone) {
+
+            // Remove whitespace from user input
+            userInput.trim();
+
+            // Checks the length
+            const phoneLength = /^\d{10}$/;
+            // Checks for valid characters
+            const validCharacters = /^[0-9-+]+$/;
+
+            let validPhone = (phoneLength.test(userInput) && validCharacters.test(userInput));
+
+            if (!validPhone) {
+
+                $(`h5#${id}`).html(`**Invalid phone number`).css("color", "red");
+                isValid = false;
 
             }
         }
+
+        if (isEmail) {
+
+
+
+        }
     });
 
-    // Handle no radio selection error
+    // Handle no radio selection 
     if (noRadioSelected) {
 
         $(`h5#${id}`).html(`**${group} is required`).css("color", "red");
