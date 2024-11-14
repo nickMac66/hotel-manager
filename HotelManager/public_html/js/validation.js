8857435af5630ca4cb35723571f55ab0a96d955e
nicmac <nick.mac.dev@outlook.com>
2024-11-13 23:27:12 -0400

    Improving validateForm function logic

--- HotelManager/public_html/js/validation.js
+++ HotelManager/public_html/js/validation.js
@@ -5,103 +5,40 @@
  * Created On: October 24, 2024
  */
 
-let isValid = true;
-
 function validateForm() {
 
-    // Constants to validate empty fields
-    const noRadioSelected = $(`input[name='${name}']:checked`).length === 0; // Check for no radio selection     
+    let isValid = true;
 
-    // Field label
-    let label = "";
+    let fname = $("#myForm #fname").val();
+    let lname = $("#myForm #lname").val();
+    let phone = $("#myForm #phone").val();
+    let email = $("#myForm #email").val();
+    let checkin = $("#myForm #checkin").val();
+    let checkout = $("#myForm #checkout").val();
+    let roomType = $('#myForm input[name="roomType"]:checked');
 
-    // Get user input & associated labels from the form
-    $("#myForm th, #myForm input").each(function () {
+    const fieldsAreEmpty = (fname === "" || lname === "" || phone === "" || email === "" || checkin === "" || checkout === "");
+    const radioNotChecked = $('#myForm input[name="roomType"]:checked').length === 0;
 
-        // Checks if the current attribute is 'label'
-        const isLabel = ($(this).is("th")); // Check if input labe
+    if (radioNotChecked) {
 
-        // Variables to hold input field attributes and values        
-        let id = $(this).attr('id');
-        let name = $(this).attr('name');
-        let group = $(this).attr('group');
-        let userInput = $(this).val();
-
-        // Checks if the current input field is for a phone number
-        let isPhone = ((id) === "phone");
-        let isEmail = ((id) === "email");
-
-        // Checks for empty fields
-        let emptyFields = (userInput === "");
-
-        // Label the input fields
-        if (isLabel) {
-
-            label = $(this).text();
-
-        }
-
-        if (isPhone) {
-
-            // Remove whitespace from user input
-            userInput.trim();
-
-            // Checks the length
-            const phonePattern = /^(?=.*[0-9-+])\d{10}$/;
-
-            let validPhone = (phonePattern.test(userInput));
-
-            if (!validPhone) {
-
-                console.log("invalid phone");
-                $(`h5#${id}`).html(`**Invalid phone number`).css("color", "red");
-                isValid = false;
-
-            } else {
-                
-                console.log("phone is valid");
-                
-            }
-        }
-
-        if (isEmail) {
-
-            console.log("is email");
-
-            // Regular Expression (Not accepts second @ symbol
-            // before the @gmail.com and accepts everything else)
-            let emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-            let valid = (emailPattern.test(userInput));
-
-            if (!valid) {
-
-                console.log("invalid email");
-                $(`h5#${id}`).html("**Invalid email address").css("color", "red");
-
-            } else {
-
-                console.log("email is valid");
-
-            }
-        }
-
-        if (emptyFields) {
-
-            $(`h5#${id}`).html(`**${label} is required`).css("color", "red");
-            isValid = false;
-
-        }
-
-    });
-
-    // Handle no radio selection 
-    if (noRadioSelected) {
-
-        $(`h5#${id}`).html(`**${group} is required`).css("color", "red");
+        $('h5[name="radioNotCheckedMsg"]').html('**Required').css("color", "red");
         isValid = false;
 
     }
+
+    $("#myForm").find("*").each(function () {
+
+        let id = $(this).attr('id');
+        let name = $(this).attr('name');
+
+        if (fieldsAreEmpty) {
+
+            $(`h5#${id}`).html('**Required').css("color", "red");
+            isValid = false;
+
+        }
+    });
 
     return isValid;
 
