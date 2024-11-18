2606731048011aa55f029a97e4624f4be18dd45e
nicmac <nick.mac.dev@outlook.com>
2024-11-18 19:08:06 -0400

    Adding connection to the database

--- HotelManager/public_html/js/dbConnection.js
+++ HotelManager/public_html/js/dbConnection.js
@@ -1,14 +1,36 @@
 let mysql = require('mysql');
 
-let con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: ""
+let conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: ""
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
+// executing connection
+conn.connect(function (err) {
+    
+    if (err) {
+     
+        console.log("error occurred while connecting");
+    
+    } else {
+    
+        console.log("connection created with mysql successfully");
+    
+    }
 });
 
-
+//const http = require('node:http');
+//
+//const hostname = '127.0.0.1';
+//const port = 3000;
+//
+//const server = http.createServer((req, res) => {
+//  res.statusCode = 200;
+//  res.setHeader('Content-Type', 'text/plain') ;
+//  res.end('Hello, World!\n');
+//});
+//
+//server.listen(port, hostname, () => {
+//  console.log(`Server running at http://${hostname}:${port}/`);
+//}); 
