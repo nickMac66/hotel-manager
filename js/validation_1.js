7dd3520442104a15e79e62791b94198e0ecdb80b
nicmac <nick.mac.dev@outlook.com>
2024-12-19 22:24:57 -0400

    Adding error msgs for invalid user input

--- js/validation_1.js
+++ js/validation_1.js
@@ -47,154 +47,35 @@ function validateForm() {
         //**********************************************************************
         switch (field) {
             case "fname":
+                if (!namePattern.test(userInput[field])) {
+                    $(`h5#${field}errorMsg`).html('**Invalid').css("color", "red");
+                    isValid = false;
+                    console.log("invalid fname or lname");
+                }
             case "lname":
                 if (!namePattern.test(userInput[field])) {
+                    $(`h5#${field}errorMsg`).html('**Invalid').css("color", "red");
                     isValid = false;
                     console.log("invalid fname or lname");
                 }
                 break;
             case "phone":
                 if (!phonePattern.test(userInput[field])) {
+                    $(`h5#${field}errorMsg`).html('**Invalid').css("color", "red");
                     isValid = false;
                     console.log("invalid phone");
                 }
                 break;
             case "email":
                 if (!emailPattern.test(userInput[field])) {
+                    $(`h5#${field}errorMsg`).html('**Invalid').css("color", "red");
                     isValid = false;
                     console.log("invalid email");
                 }
                 break;
-            default:
-                console.log("default value");
+            default:                
                 break;
         }
     }
     return isValid;
 }
-
-
-//
-//                const fname = $("#myForm #fname").val();
-//                const fnamepattern = /^[a-zA-Z]{1,50}$/;
-//                const fnameInvalid = (!fnamepattern.test(fname));
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required field').css("color", "red");
-//                    console.log("first name is empty");
-//                    isValid = false;
-//
-//                } else if (fnameInvalid) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Invalid').css("color", "red");
-//                    console.log("first name is invalid");
-//                    isValid = false;
-//
-//                }
-//                                
-//                break;
-//
-//            case "lname":
-//
-//                const lname = $("#myForm #lname").val();
-//                const lnamePattern = /^[a-zA-Z]{1,50}$/;
-//                const lnameInvalid = (!lnamePattern.test(lname));
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required').css("color", "red");
-//                    console.log("last name is empty");
-//                    isValid = false;
-//
-//                } else if (lnameInvalid) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Invalid').css("color", "red");
-//                    console.log("first name is invalid");
-//                    isValid = false;
-//
-//                }
-//
-//                break;
-//
-//            case "phone":
-//                
-//                const phone = $("#myForm #phone").val();
-//                const phonePattern = /^\d{10}$/;                
-//                const phoneInvalid = (!phonePattern.test(phone));
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required').css("color", "red");
-//                    console.log("phone is empty");
-//                    isValid = false;
-//
-//                } else if (phoneInvalid) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Invalid').css("color", "red");
-//                    console.log("phone is invalid");
-//                    isValid = false;
-//
-//                }
-//
-//                break;
-//
-//            case "email":
-//
-//                const email = $("#myForm #email").val();
-//                const emailPattern = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;                
-//                const emailInvalid = (!emailPattern.test(email));
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required').css("color", "red");
-//                    console.log("email is empty");
-//                    isValid = false;
-//
-//                } else if (emailInvalid) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Invalid').css("color", "red");
-//                    console.log("email is invalid");
-//                    isValid = false;
-//
-//                }
-//
-//                break;
-//
-//            case "checkin":
-//
-//                const checkin = $("#myForm #checkin").val();
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required').css("color", "red");
-//                    console.log("check in is empty");
-//                    isValid = false;
-//
-//                }
-//
-//                break;
-//
-//            case "checkout":
-//
-//                const checkout = $("#myForm #checkout").val();
-//
-//                if (isEmpty) {
-//
-//                    $(`h5#${id}errorMsg`).html('**Required').css("color", "red");
-//                    console.log("checkout is empty");
-//                    isValid = false;
-//
-//                }
-//
-//                break;
-//
-//            default:
-//
-//                break;
-//
-//        }
-//    });
-//    console.log("validated");
-//    return isValid;
-//}
