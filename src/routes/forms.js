24944ceb193cc9f86bae5dcf9037a37fcb8acb88
nicmac <nick.mac.dev@outlook.com>
2025-01-11 01:49:11 -0400

    Cleaning up form route code

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -1,51 +1,44 @@
-// Import express module and create Express server
+/*
+ * Name:        forms.js
+ * Description: Route handlers for displaying and submitting booking forms
+ * Author:      NicMac
+ * Date:        January 11, 2025
+ */
+
+// Importing Express modules
 const express = require('express');
-
-// Importing the path module for working with file and directory paths
-const path = require('path');
-
-// Resoloving the path to the index.ejs view file
-const index = path.resolve(__dirname, '../views/index.ejs');
-
-// Creating Express router
-const router = express.Router();
-
-// Importing the body-parser module to parse request bodies
 const bodyParser = require('body-parser');
-
-// Middleware to parse JSON-encoded bodies
-router.use(bodyParser.json());
-
-// Middleware to parse URL-encoded bodies
-router.use(bodyParser.urlencoded({extended: true}));
 
 // Importing the buildForm function from the bookingForm module
 const {buildForm} = require('../models/bookingForm');
 
+// Creating an instance of an Express router
+const router = express.Router();
+
+// Middleware to parse JSON-encoded bodies
+router.use(bodyParser.json());
+// Middleware to parse URL-encoded bodies
+router.use(bodyParser.urlencoded({extended: true}));
+
 // Route to display the booking form
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
     console.log("hello from the form route");
     let html = buildForm();
     res.render("index", {html});
 });
 
-router.post('/', (req, res, next) => {
-    console.log("...form submitted...");
+// Route to handle form submission
+router.post('/', (req, res) => {    
     // Get user input data from the form
-    const fname = req.body.fname;
-    console.log("first name: " + fname);
-    const lname = req.body.lname;
-    console.log("last name: " + lname);
-    const phone = req.body.phone;
-    console.log("phone: " + phone);
-    const email = req.body.email;
-    console.log("email: " + email);
-    const checkin = req.body.checkin;
-    console.log("checkin: " + checkin);
-    const checkout = req.body.checkout;
-    console.log("checkout: " + checkout);
-    const roomType = req.body.roomType;
-    console.log("room type: " + roomType);
+    const {fname, lname, phone, email, checkin, checkout, roomType} = req.body;
+    console.log("first name:", fname);
+    console.log("last name:", lname);
+    console.log("phone:", phone);
+    console.log("email:", email);
+    console.log("checkin:", checkin);
+    console.log("checkout:", checkout);
+    console.log("room type:", roomType);
+    res.send('Form submitted successfully');
 });
 
 // Exporting the router module so it can be used in other files
