72bbb8a0c2e2f6e761c3d0751762d017ac3ead4a
nicmac <nick.mac.dev@outlook.com>
2025-02-05 23:01:10 -0400

    Code cleanup - cleaning up  buildForm function logic

--- src/models/bookingForm.js
+++ src/models/bookingForm.js
@@ -23,65 +23,24 @@ const formObject = {
 const buildForm = (bookingDetails = {}, formAction) => {
     let form = `<form id="bookingForm" action="${formAction}" method="post"><table>`;
 
-    if (Object.keys(bookingDetails).length > 0) {
-        console.log(bookingDetails.book);
+    // If updating a booking, pre-fill the 'booking id' field
+    if (formAction === 'update') {
         form += `<tr><th><label for='bookingId'>Booking Id</label></th>`;
         form += `<td><input type='text' id='bookingId' name='bookingId' value='${bookingDetails.booking._id}'></td>`;
     }
 
+    // Add form fields
     for (let key in formObject) {
-        let attribute = formObject[key]
+        let { label, input, id, name } = formObject[key];
+        let value = bookingDetails[key] || '';
 
-        form += `<tr><th><label for='bookingId'>${attribute.label}</label></th>`;
-        form += `<td><input type='${attribute.input}' id='${attribute.id}' name='${attribute.name}' value='${attribute.value}'></td>`;
+        form += `<tr><th><label for='${id}'>${label}</label></th>`;
+        form += `<td><input type='${input}' id='${id}' name='${name}' value='${value}'></td>`;
     }
 
-    // if (formAction === 'update') {
-    //     form += `<tr><th><label for='bookingId'>Booking number</label></th>`;
-    // form += `<td><input type='text' id='bookingId' name='bookingId' value='${bookingDetails.booking._id}' readonly></td>`;
-    //     // form += `<td name='id'>${bookingDetails.booking._id}</td></tr>`;
-
-    // }
-
-    // // Initialize an object representing each form field with its attributes
-    // for (let key in formObject) {
-
-    //     let fieldObject = formObject[key];
-
-    //     // Initialize attributes for the current form field & add them to the form
-    //     for (let key in fieldObject) {
-
-    //         switch (key) {
-    //             case "label":
-    //                 const label = fieldObject[key];
-    //                 form += `<tr><th><label for='${fieldObject.id}'>${label}</label></th>`;
-    //                 break;
-    //             case "input":
-
-    //                 // Populate the form fields with booking details if updating an existing booking
-    //                 if (formAction === 'update') {
-
-    //                     let fieldId = fieldObject.id;
-    //                     let fieldValue = bookingDetails.booking[fieldId];
-
-    //                     form += `<td><input type='${fieldObject.input}' id='${fieldObject.id}' name='${fieldObject.name}' value='${fieldValue}'</td>`;
-
-    //                 } else {
-    //                     // No value attribute needed for new booking
-    //                     form += `<td><input type='${fieldObject.input}' id='${fieldObject.id}' name='${fieldObject.name}'</td>`;
-    //                     form += `<td><span id='${fieldObject.id}+ErrMsg' style='display:none;'>**Required field</span></td></tr>`;
-    //                 }
-    //             default:
-    //                 "invalid attribute";
-    //         }
-    //     }
-    // }
     // Add the submit button and close the form
     form += `<tr><td colspan="3"><input type="submit" action="/${formAction}" id="submitButton" name="submitButton"></td></tr></table>`;
-
     return form;
 };
 
-module.exports = {
-    buildForm
-};
+module.exports = { buildForm };
